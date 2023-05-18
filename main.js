8e3f6fe3220fae2d8c4f181cae4707a128092dc7
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-05-18 12:09:39 -0700

    Save Table Button implementation

--- main.js
+++ main.js
@@ -3,6 +3,53 @@ import createTable from './createTable.js';
 import fetchData from './fetchData.js';
 import filterTable from './filterTable.js';
 
+function downloadCSV(csv, filename) {
+	let csvFile;
+	let downloadLink;
+
+	// CSV File
+	csvFile = new Blob([csv], {
+		type: 'text/csv',
+	});
+
+	// Download link
+	downloadLink = document.createElement('a');
+
+	// File name
+	downloadLink.download = filename;
+
+	// Create a link to the file
+	downloadLink.href = window.URL.createObjectURL(csvFile);
+
+	// Hide link
+	downloadLink.style.display = 'none';
+
+	// Add link to the DOM
+	document.body.appendChild(downloadLink);
+
+	// Click the link
+	downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+	let csv = [];
+	let rows = document.querySelectorAll('table tr');
+
+	for (let i = 0; i < rows.length; i++) {
+		let row = [];
+		let cols = rows[i].querySelectorAll('td, th');
+
+		for (let j = 0; j < cols.length; j++) {
+			row.push(cols[j].innerText);
+		}
+
+		csv.push(row.join(','));
+	}
+
+	// Download CSV
+	downloadCSV(csv.join('\n'), filename);
+}
+
 const init = async (url) => {
 	const tableBody = document.querySelector('tbody[data-tbody-id="data"]');
 
@@ -63,5 +110,11 @@ const init = async (url) => {
 		});
 	});
 };
+
+const saveBtn = document.querySelector('#save-table');
+saveBtn.addEventListener('click', () => {
+	let filename = prompt("Save as:", "");
+	if (filename) exportTableToCSV(`${filename}.csv`);
+})
 
 init('https://jsonplaceholder.typicode.com/users');
